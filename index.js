ea969eb0355868fc20ad6a5df0bea84b58dab269
pablo.silva <pablo.silva@doclerholding.com>
2018-10-22 08:20:19 +0200

    feat(index): refactored file for better readability

--- index.js
+++ index.js
@@ -2,77 +2,97 @@
 
 import { argv } from 'yargs'
 
-const a = argv._
-const task = a[0]
-
-switch (task) {
-	case 'boot': {
-	    const boot = require('@mhy/boot')
-        const output = argv.o || argv.output
-		boot(a[1], a[2], output)
-		break
-	}
-    case 'config': {
-        const load = require('@mhy/config').load
-    	const config = a[1]
-		const format = argv.f || argv.format || 'js'
-        if (!config) {
-			console.error('No config specified!')
-			process.exit(2)
-        }
-        let result = JSON.stringify(load(config), null, 2)
-		if (format === 'js') {
-			result = `module.exports = module.exports.default = ${result}`
-		}
-        process.stdout.write(result)
-        process.exit(0)
-		break
-    }
-	case 'ui':
-	case undefined: {
-        const load = require('@mhy/config').load
-	    const eco = load('ecosystem')
-        process.MHY_ENV = 'ui'
-        if (!Object.keys(eco).length) {
-            console.error('No UI Widgets are specified in the Ecosystem.')
-            process.exit(2)
-        }
-        const { disabled = [] } = load('ui');
-        // Run processes
-        const processes = Object.entries(eco)
-        .filter(([ key ]) => !disabled.includes(key))
-        .sort(([ , { order: ao = 0 } ], [ , { order: bo = 0 } ]) => ao - bo) // static order
-        .reduce((o, [ name, Process ]) => {
-            o[ name ] = new Process()
-            return o
-        }, {})
-        // Init magic
-        const render = require('./ui').default
-        render(processes)
-        break
-    }
-	default:
-	case 'run': {// mhy webpack
-        const load = require('@mhy/config').load
-        const eco = load('ecosystem')
-        if (task !== 'run' && !(task in eco)) {
-            console.error(`No such task: ${task}`)
-            process.exit(2)
-        }
-        process.MHY_ENV = 'cli'
-        let proc, args
-        if (task === 'run') {
-            [ , proc, ...args ] = a
-        }
-        else {
-            [ proc, ...args ] = a
-        }
-        const Process = eco[ proc ]
-        if (!Process) {
-            console.error(`No such process: ${proc}`)
-            process.exit(2)
-        }
-        (new Process(...args)).on('data', l => console.log(l))
-        break
+function getLoadEco() {
+    const load = require('@mhy/config').load
+    const eco = load('ecosystem')
+    return {
+        load,
+        eco
     }
 }
+
+function processExit(exitSignal, message) {
+    console.error(message)
+    process.exit(exitSignal)
+}
+
+function bootTask(technology, template, output) {
+    const boot = require('@mhy/boot')
+    boot(technology, template, output)
+}
+
+function configTask(config, format) {
+    const load = require('@mhy/config').load
+    if (!config) {
+        return processExit(2, 'No config specified!')
+    }
+    let result = JSON.stringify(load(config), null, 2)
+    if (format === 'js') {
+        result = `module.exports = module.exports.default = ${result}`
+    }
+    process.stdout.write(result)
+    process.exit(0)
+}
+
+function uiTask() {
+    const { load, eco } = getLoadEco()
+    process.MHY_ENV = 'ui'
+    if (!Object.keys(eco).length) {
+        return processExit(2, 'No UI Widgets are specified in the Ecosystem.')
+    }
+    const { disabled = [] } = load('ui');
+    // Run processes
+    const processes = Object.entries(eco)
+        .filter(([key]) => !disabled.includes(key))
+        .sort(([, { order: ao = 0 }], [, { order: bo = 0 }]) => ao - bo) // static order
+        .reduce((o, [name, Process]) => {
+            o[name] = new Process()
+            return o
+        }, {})
+    // Init magic
+    const render = require('./ui').default
+    render(processes)
+}
+
+function runTask(cmdArguments) {
+    const { eco } = getLoadEco()
+    if (task !== 'run' && !(task in eco)) {
+        return processExit(2, `No such task: ${task}`)
+    }
+    process.MHY_ENV = 'cli'
+    let proc, args
+    if (task === 'run') {
+        [, proc, ...args] = cmdArguments
+    }
+    else {
+        [proc, ...args] = cmdArguments
+    }
+    
+    const Process = eco[proc]
+    if (!Process) {
+        return processExit(2, `No such process: ${proc}`)
+    }
+
+    (new Process(...args)).on('data', l => console.log(l))
+}
+
+function init(argv, [task, ...cmdArgumentsArray]) {
+    switch (task) {
+        case 'boot': {
+            return bootTask(cmdArgumentsArray[0], cmdArgumentsArray[1], argv.o || argv.output)
+        }
+        case 'config': {
+            return configTask(cmdArgumentsArray[0], argv.f || argv.format || 'js')
+        }
+        case 'ui':
+        case undefined: {
+            return uiTask()
+        }
+        case 'run':
+        default: {// mhy webpack
+            return runTask(cmdArgumentsArray)
+        }
+    }
+}
+
+init(argv, argv._)
